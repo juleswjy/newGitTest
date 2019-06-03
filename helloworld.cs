541cc57cdd1af9cfb81ea40211d866c3111dd216
Julian Woo <Julian.Woo@citrix.com>
2019-06-03 10:05:30 -0400

    added main function

--- helloworld.cs
+++ helloworld.cs
@@ -4,10 +4,14 @@ public class TestClass
 {
 	public TestClass()
 	{
-        Console.WriteLine("Hello World!");
+        public Main()
+        {
+            Console.WriteLine("Hello World!");
 
-        Console.WriteLine("This exists in develop2 branch");
+            Console.WriteLine("This exists in develop2 branch");
 
-        Console.WriteLine("Extra line in develop2 branch");
+            Console.WriteLine("Extra line in develop2 branch");
+        }
+        
 	}
 }
